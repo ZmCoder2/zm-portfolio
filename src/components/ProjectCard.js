824e96021ff67c616921a436f4d6ffbf27176881
Zack Miller <126508376+ZmCoder2@users.noreply.github.com>
2024-04-11 22:29:28 -0400

    project cards

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,38 +1,35 @@
 import React from 'react';
 import { Card, Button } from "react-bootstrap";
 
-// Import projectsData from projects.json
-import projectsData from "../../src/projects.json";
-
-const ProjectCard = ({ isVisible }) => {
-    return (
-        <>
-            {projectsData.map((project, index) => (
-                <Card key={index} className={`mb-4 ${isVisible ? "animate__animated animate__fadeIn" : ""}`}>
-                    {project.videoUrl ? (
-                        <iframe
-                            src={project.videoUrl.replace("/view?usp=sharing", "/preview")}
-                            title={project.name}
-                            className="projVideo"
-                            allow="autoplay"
-                            allowFullScreen
-                        />
-                    ) : (
-                        <Card.Img variant="top" src={project.videoUrl} alt={project.name} />
-                    )}
-                    <Card.Body>
-                        <Card.Title>{project.name}</Card.Title>
-                        <Card.Text>{project.description}</Card.Text>
-                        <div>
-                            <Button variant="primary" href={project.github}>Repo</Button>
-                            <Button variant="secondary" href={project.live}>Live</Button>
-                        </div>
-                    </Card.Body>
-                </Card>
-            ))}
-        </>
-    );
+const ProjectCard = ({ project }) => {
+  console.log(project);
+  // Remove isVisible prop from Card element
+  return (
+    <Card className="project-card mb-4 animate__animated animate__fadeIn">
+      {project.video ? (
+        <iframe
+          src={project.video.replace("/view?usp=sharing", "/preview")}
+          title={project.name}
+          className="projVideo"
+          allow="autoplay"
+          allowFullScreen
+        />
+      ) : (
+        <Card.Img variant="top" src={project.imageUrl} alt={project.name} />
+      )}
+      <Card.Body className="project-card-body">
+        <div className="project-card-title">{project.name}</div>
+        {/* <div className="project-card-description">{project.description}</div> */}
+        <div className="project-card-buttons">
+          <Button variant="primary" href={project.github}>Repo</Button>
+          <Button variant="secondary" href={project.live}>Live</Button>
+        </div>
+      </Card.Body>
+    </Card>
+  );
 }
 
 export default ProjectCard;
+
+
 
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,35 +1,39 @@
 import React from 'react';
-import { Container, Row, Col, Tab } from "react-bootstrap";
+import { Container, Row, Col } from "react-bootstrap";
 import 'animate.css';
 import TrackVisibility from "react-on-screen";
-import ProjectCard from "../components/ProjectCard"; // Import the updated ProjectCard component
+import ProjectCard from "../components/ProjectCard";
+import projects from "../../src/projects.json";
 
 export const Projects = () => {
-    return (
-        <section className="project" id="projects">
-            <Container>
-                <Row>
-                    <Col size={12}>
-                        <TrackVisibility>
-                            {({ isVisible }) => (
-                                <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
-                                    <h2>Projects</h2>
-                                    <p>Here are some projects that I am proud of:</p>
-                                    <Tab.Content id="slideInUp" className={isVisible ? "animate__animated animate__slideInUp" : ""}>
-                                        <Tab.Pane eventKey="first">
-                                            <Row>
-                                                <ProjectCard isVisible={isVisible} /> {/* Pass isVisible prop to ProjectCard */}
-                                            </Row>
-                                        </Tab.Pane>
-                                    </Tab.Content>
-                                </div>
-                            )}
-                        </TrackVisibility>
-                    </Col>
-                </Row>
-            </Container>
-        </section>
-    );
+  return (
+    <section className="project" id="projects">
+      <Container>
+        <Row>
+          <Col size={12}>
+            <TrackVisibility>
+              {({ isVisible }) => (
+                <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
+                  <h2>Projects</h2>
+                  <p>Here are some projects that I am proud of:</p>
+                  <Row>
+                    {projects.map((project, index) => (
+                      <Col md={6} key={index}>
+                        <ProjectCard project={project} />
+                      </Col>
+                    ))}
+                  </Row>
+                </div>
+              )}
+            </TrackVisibility>
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  );
 }
+
+
+
 
 
